a60bf1fc9760c5b5d0d8bf30024abc580196d9be
Farzeen12 <farzeenharris900@gmail.com>
2024-11-23 19:34:34 +0530

    first commit

---
+++ nfs/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def NFS(request):
+    return HttpResponse('NFS mostwanted is the best game in the world')
